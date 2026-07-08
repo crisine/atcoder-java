ac238587818eb60521816b7b42fe6ccb463a9180
misung <crisine@knou.ac.kr>
2026-07-08 22:13:38 +0900

    solve: math-and-algorithm 015を解く

---
+++ contests/algomath/z015/Main.java
@@ -0,0 +1,22 @@
+package algomath.z015;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int answer = 0;
+        for (int i = 2; i <= a; i++) {
+            if (a % i == 0) {
+                if (b % (a / i) == 0) {
+                    answer = a / i;
+                    break;
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
